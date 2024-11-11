be234acf5c770c732c1c0f59c41202894bb00eb7
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-11-11 15:46:04 +0000

    FEAT : adds further npm config

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,18 +1,29 @@
 import { cdk, javascript } from 'projen';
+import { NpmAccess } from 'projen/lib/javascript';
 const project = new cdk.JsiiProject({
+  // packageName: "@alanionita/scaffold-projen-cdk",  /* The "name" in package.json. */
   author: 'Alan Ionita',
   authorAddress: '105461667+alanionita@users.noreply.github.com',
   defaultReleaseBranch: 'main',
+  deps: ['aws-lambda', 'axios'],
+  description: 'Projen template for AWS CDK micro-services',
+  devDeps: ['@types/aws-lambda', '@types/node', 'ts-node'],
+  githubOptions: {
+    mergify: false,
+  },
   jsiiVersion: '~5.5.0',
   name: 'scaffold-projen-cdk',
-  projenrcTs: true,
-  // packageName: "@alanionita/scaffold-projen-cdk",  /* The "name" in package.json. */
+  npmAccess: NpmAccess.PUBLIC,
+  npmProvenance: false,
   packageManager: javascript.NodePackageManager.NPM,
+  peerDeps: ['constructs', 'projen'],
+  projenrcTs: true,
   releaseToNpm: true,
   repositoryUrl: 'https://github.com/alanionita/scaffold-projen-cdk-ts',
-  description: 'Projen template for AWS CDK micro-services',
-  peerDeps: ['constructs', 'projen'],
-  deps: ['aws-lambda', 'axios'],
-  devDeps: ['@types/aws-lambda', '@types/node', 'ts-node'],
 });
+
+const exclusions: string[] = ['.env'];
+project.gitignore.exclude(...exclusions);
+project.npmignore!.exclude(...exclusions);
+
 project.synth();
