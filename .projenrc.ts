c8bb8b930aa0348858fca1827067128b743db3db
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-11-11 15:58:28 +0000

    FIX: typo

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,7 +1,7 @@
 import { cdk, javascript } from 'projen';
 import { NpmAccess } from 'projen/lib/javascript';
 const project = new cdk.JsiiProject({
-  packageName: "@alanionita/scaffold-projen-cdk-ts",  /* The "name" in package.json. */
+  packageName: "@alanionita/scaffold-projen-cdk-ts",
   author: 'Alan Ionita',
   authorAddress: '105461667+alanionita@users.noreply.github.com',
   bundledDeps: ['aws-lambda', 'axios'],
